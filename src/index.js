1f91f78e682886559428551b7326e732c9857ecd
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-05 21:08:34 +0600

    connected successfully mongodb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+import "dotenv/config";
+// dotenv.config({ path: "./env" });
+import connectDB from "./db/db.js";
 
+connectDB();
+
+/* import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_name}`);
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+  } catch (error) {
+    console.error("Error: DB connecting error", error);
+    throw error;
+  }
+})();
+*/
