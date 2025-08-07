9a7d21f57b08d238a19296e315e1e29cf5619e1b
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-07 23:30:32 +0600

    added a method to uploadOnCloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,21 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECERET,
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // uplodaing file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded on cloudinary successfully
+    console.log("File is uploaded on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // removed the temporary localFilePath file as the operation got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
