2059d4c82111b9ecb2b9227a289d494200c4c196
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-07 23:11:39 +0600

    added cloud name

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,7 +2,7 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
-  cloud_name: "dkfa97zjo",
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECERET,
 });
