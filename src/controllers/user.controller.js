11ff9a309efa1cc536cdfb13ac2e5b7d0882b39e
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-09-11 18:13:57 +0600

    write down the steps

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,15 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  // get req data
+  // validation - not empty
+  // check if user already exist: username, emai,
+  // check image check for avatar
+  // upload them to cloudinary
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
 });
 
 export { registerUser };
