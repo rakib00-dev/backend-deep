d2161fc581f4f6ac6fffa9075b6d09c0aa9fb174
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-09-14 14:22:32 +0600

    removed the comment

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -45,9 +45,6 @@ const registerUser = asyncHandler(async (req, res) => {
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
-  console.log(`avatarLocalPath ${avatarLocalPath}`);
-  console.log(`avatar ${avatar}`);
-
   if (!avatar) {
     throw new ApiError(400, "Avatar file is required");
   }
