1ec83c095307add5afeb00a63c0e90f8ddd178df
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-09-15 16:00:31 +0600

    Fixed the apiresponse

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,5 +1,5 @@
 class ApiResponse {
-  constructor(message = "success", statusCode, data) {
+  constructor(statusCode, data, message = "success") {
     this.statusCode = statusCode;
     this.data = data;
     this.message = message;
