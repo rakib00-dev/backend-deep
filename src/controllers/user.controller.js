934876d02e72d6cebc3dc449aed916201f2a0bca
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-09-18 19:03:30 +0600

    fixed controller.js some controler

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -458,13 +458,7 @@ const getWatchHistory = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .json(
-      new ApiResponse(
-        200,
-        user[0]?.watchHistory,
-        "watch history fetched successfully"
-      )
-    );
+    .json(new ApiResponse(200, user, "watch history fetched successfully"));
 });
 
 export {
