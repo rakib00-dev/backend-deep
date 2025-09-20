3dd93133aafa3a4f8d4c3c89a6a0240bafc46c5e
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-09-20 10:07:18 +0600

    add some controllers in playlist controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -31,20 +31,101 @@ const createPlaylist = asyncHandler(async (req, res) => {
 const getUserPlaylists = asyncHandler(async (req, res) => {
   const { userId } = req.params;
   //TODO: get user playlists
+
+  if (!userId) {
+    throw new ApiError(400, "Must have userID to get user playlist");
+  }
+
+  const UserPlaylist = await Playlist.aggregate([
+    {
+      $match: {
+        owner: userId,
+      },
+    },
+  ]);
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, UserPlaylist, "Fetched User Playlist Successfully")
+    );
 });
 
 const getPlaylistById = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
+
   //TODO: get playlist by id
+
+  if (!playlistId) {
+    throw new ApiError(400, "Must Enter Playlist id to find playlist");
+  }
+
+  const playlist = await Playlist.findById(playlistId);
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Playlist fetched successfully"));
 });
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
   const { playlistId, videoId } = req.params;
+
+  if (!(playlistId && videoId)) {
+    throw new ApiError(400, "PlaylistId and VideoId must exist to add video");
+  }
+
+  const addedVideo = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $set: {
+        videos: [videoId],
+      },
+    },
+    { new: true }
+  );
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        addVideoToPlaylist,
+        "Added video to playlist successfully"
+      )
+    );
 });
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
   const { playlistId, videoId } = req.params;
+
   // TODO: remove video from playlist
+
+  if (!(playlistId && videoId)) {
+    throw new ApiError(
+      400,
+      "Must Have PlaylistId and videoId to remove video from playlist"
+    );
+  }
+
+  const removedVideoFromPlaylist = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $pull: {
+        videos: videoId,
+      },
+    },
+    { new: true }
+  );
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        removedVideoFromPlaylist,
+        "Removed video from playlist successfully"
+      )
+    );
 });
 
 const deletePlaylist = asyncHandler(async (req, res) => {
