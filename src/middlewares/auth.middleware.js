6854a1d483d8de135f8bce7c109ff7e44d870a2f
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-09-21 06:08:25 +0600

    updated the auth middleware console log

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -11,7 +11,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
 
     //  console.log(token);
     if (!token) {
-      throw new ApiError(401, "Unauthorized request");
+      throw new ApiError(401, "Unauthorized request, Please Login");
     }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
