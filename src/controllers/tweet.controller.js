acf22f79fb99eda22dee2501286127bb0138661e
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-09-22 10:44:26 +0600

    added tweet controller login and now it's time for test

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,29 +1,99 @@
-import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-})
+  //TODO: create tweet
+
+  const { content } = req.body;
+  const { _id: userID } = req.user;
+
+  const isUserIdValid = isValidObjectId(userID);
+
+  if (!content) {
+    throw new ApiError(400, "content must exist to ");
+  }
+
+  if (!userID) {
+    throw new ApiError(400, "User must have logged to add tweet");
+  }
+
+  if (!isUserIdValid) {
+    throw new ApiError(400, "User Id is not valid");
+  }
+
+  const tweet = await Tweet.create({ content, owner: userID });
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Successfully Created new tweet"));
+});
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
-})
+  // TODO: get user tweets
+
+  const { userId } = req.params;
+
+  if (!userId) {
+    throw new ApiError(400, "Failed to find user");
+  }
+
+  const userTweets = await Tweet.find({ owner: userId });
+
+  if (!userTweets) {
+    throw new ApiError(200, "Can not find user tweets");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, userTweets, "Successfuly get all user tweets"));
+});
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
-})
+  //TODO: update tweet
+
+  const { tweetId } = req.params;
+  const { content } = req.body;
+
+  if (!tweetId) {
+    throw new ApiError(404, "Failded to get tweet id");
+  }
+
+  if (!content) {
+    throw new ApiError(404, "Failded to get content to update tweet");
+  }
+
+  const tweet = await Tweet.findByIdAndUpdate(
+    tweetId,
+    { $set: { content } },
+    { new: true }
+  );
+
+  if (!tweet) {
+    throw new ApiError(400, "Tweet not exist");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Successfully updated the tweet"));
+});
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
-})
+  //TODO: delete tweet
+  const { tweetId } = req.params;
 
-export {
-    createTweet,
-    getUserTweets,
-    updateTweet,
-    deleteTweet
-}
+  const isTweetIdValid = isValidObjectId(tweetId);
+
+  if (!tweetId || !isTweetIdValid) {
+    throw new ApiError(400, "Faild to get tweet id or it is not valid");
+  }
+
+  await Tweet.findByIdAndDelete(tweetId);
+
+  res.status(200).json(new ApiResponse(200, {}, "Successfully deleted tweet"));
+});
+
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
