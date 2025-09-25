33a73795d34fc7a181a505d42ca420d36d705381
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-09-25 11:47:49 +0600

    finished all video controllers accepect one getAllVideos by filter now I am doing all of that

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -11,12 +11,51 @@ import {
 
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+
   //TODO: get all videos based on query, sort, pagination
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
-  // TODO: get video, upload to cloudinary, create video
+  const { _id: userId } = req.user;
+
+  // TODO✅: get video, upload to cloudinary, create video
+
+  if (!userId || !isValidObjectId(userId)) {
+    throw new ApiError("User Must login");
+  }
+
+  const videoLocalPath = req.files?.videoFile[0]?.path;
+  const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+
+  if (!videoLocalPath) {
+    throw new ApiError(400, "video must select to upload");
+  }
+
+  if (!thumbnailLocalPath) {
+    throw new ApiError(400, "thumbnail must select to upload");
+  }
+
+  if (!title) {
+    throw new ApiError(400, "required the title field");
+  }
+
+  const videoPublic = await uploadOnCloudinary(videoLocalPath);
+  const thumbnailPublic = await uploadOnCloudinary(thumbnailLocalPath);
+
+  const video = await Video.create({
+    title,
+    description: description || "",
+    duration: videoPublic.duration,
+    videoFile: videoPublic.url,
+    thumbnail: thumbnailPublic.url,
+    owner: userId,
+    isPublished: true,
+  });
+
+  res
+    .status(201)
+    .json(new ApiResponse(201, video, "Video created successfully"));
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
@@ -28,7 +67,11 @@ const getVideoById = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Video id is not found or not valid");
   }
 
-  const video = await Video.findById(videoId);
+  const video = await Video.findByIdAndUpdate(
+    videoId,
+    { $inc: { views: 1 } },
+    { new: true }
+  );
 
   if (!video) {
     throw new ApiError(404, "video not found");
@@ -41,7 +84,50 @@ const getVideoById = asyncHandler(async (req, res) => {
 
 const updateVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
+  const { title, description } = req.params;
+
   //TODO: update video details like title, description, thumbnail
+
+  if (!videoId || !isValidObjectId(videoId)) {
+    throw new ApiError(400, "Video is not found or not valid");
+  }
+
+  if ([title, description].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const oldThumbnail = await Video.findById(videoId);
+  const oldThumbnailPath = oldThumbnail.thumbnail;
+
+  const newThumbnailPath = req.file?.path;
+
+  if (!newThumbnailPath) {
+    throw new ApiError(400, "thumbnail file could not found");
+  }
+
+  const thumbnail = await uploadOnCloudinary(newThumbnailPath);
+
+  const newVideo = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: {
+        title,
+        description,
+        thumbnail: thumbnail.url,
+      },
+    },
+    { new: true }
+  );
+
+  if (oldThumbnailPath) {
+    await destroyOnCloudinary(oldThumbnailPath);
+  }
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, newVideo, "Successfully updated the video details")
+    );
 });
 
 const deleteVideo = asyncHandler(async (req, res) => {
@@ -62,8 +148,13 @@ const deleteVideo = asyncHandler(async (req, res) => {
     throw new ApiError(404, "Video not found");
   }
 
-  await destroyOnCloudinary(video.videoFile);
-  await destroyOnCloudinary(video.thumbnail);
+  if (video.videoFile) {
+    await destroyOnCloudinary(video.videoFile, "video");
+  }
+
+  if (video.thumbnail) {
+    await destroyOnCloudinary(video.thumbnail);
+  }
 
   res
     .status(200)
